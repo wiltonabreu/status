6fc2d957e60ce2896d3bb1e78be341e4e61417e4
Wilton <wiltonabreuu@gmail.com>
2019-02-17 11:40:41 -0300

    CRUD concluido

--- index.php
+++ index.php
@@ -109,5 +109,100 @@ $app->get('/admin/logout',function () {
 	
 });
 
+
+$app->get('/admin/users',function () {
+	
+	User::verifylogin();
+
+	$users = User::listAll();
+
+	$page = new PageAdmin();
+	$page->setTpl("users", array(
+		"users"=>$users
+	));
+	
+});
+
+$app->get('/admin/users/create',function () {
+	
+	User::verifylogin();
+
+	$page = new PageAdmin();
+	$page->setTpl("users-create");
+	
+});
+
+$app->get('/admin/users/:iduser/delete', function($iduser){
+	User::verifylogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$user->delete();
+
+	header("Location: /admin/users");
+
+	exit;
+
+
+});
+
+$app->get('/admin/users/:iduser',function($iduser) {
+	
+	User::verifylogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-update", array(
+		"user"=>$user->getValue()
+	));
+	
+});
+
+$app->post('/admin/users/create',function() {
+	
+	User::verifylogin();
+	
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->setData($_POST);
+
+	$user->save();
+
+	header("Location: /admin/users");
+
+	exit;
+	
+});
+
+$app->post('/admin/users/:iduser',function ($iduser) {
+	
+	User::verifylogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->get((int) $iduser);
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header("Location: /admin/users");
+
+	exit;
+	
+});
+
+
+
 $app->run();
 ?>
