c52a67835f6864745bdd3b6886b32929fa176a9d
Wilton <wiltonabreuu@gmail.com>
2019-02-25 12:59:32 -0300

    Esqueci a senha

---
+++ vendor/classes/php-classes/src/Mailer.php
@@ -0,0 +1,84 @@
+<?php
+namespace Classes;
+
+use \Rain\Tpl;
+use \PHPMailer;
+
+class Mailer{
+
+    const USERNAME = "wilton@mav.com.br";
+    const HOSTNAME = "mail.mav.com.br";
+    const PASSWORD = "123.qwe";
+    const NAME_FROM = "MAV Tecnologia";
+
+    private $mail;
+
+    public function __construct($toAdrress, $toName, $subject, $tlpName, $data = array()){
+    
+        $config = array(
+			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/email/",
+			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+			"debug"         => false
+	    );
+
+		Tpl::configure( $config );
+
+        $tpl = new Tpl;
+
+        
+        foreach ($data as $key => $value) {
+            $tpl->assign($key,$value);
+        }
+        
+        $html = $tpl->draw($tlpName, true);
+
+        $this->mail = new PHPMailer(); 
+        $this->mail->IsSMTP();
+        $this->mail->SMTPDebug = 2;
+        $this->mail->Host = Mailer::HOSTNAME;
+        $this->mail->SMTPAuth = true;
+        $this->mail->Port = 587;
+
+        
+
+
+        //==================$this->mail->SMTPAutoTLS = false========================
+
+        $this->mail->SMTPAutoTLS = false;
+
+        
+        $this->mail->Username = Mailer::USERNAME; 
+        $this->mail->Password = Mailer::PASSWORD; 
+        
+        //Define os Remetente 
+        $this->mail->setFrom(Mailer::USERNAME,Mailer::NAME_FROM); 
+        
+        //Define os destinatário(s)     
+        $this->mail->AddAddress($toAdrress);
+
+       
+        //$this->mail->AddAddress('usuario_destino@dominio.com.br');
+        
+
+        $this->mail->IsHTML(true); 
+        $this->mail->CharSet = 'UTF-8'; // Charset da mensagem (opcional) 
+
+        //Assunto do E-mail
+        $this->mail->Subject  = $subject;
+        $this->mail->msgHTML($html);
+
+
+        //Corpo do E-mail
+        $this->mail->AltBody = 'Teste de envio de e-mail';
+
+        
+        
+    
+    }
+
+    public function send(){
+       return $this->mail->send();
+        
+    }
+}
+?>
--- vendor/classes/php-classes/src/Model/User.php
+++ vendor/classes/php-classes/src/Model/User.php
@@ -4,10 +4,12 @@ namespace Classes\Model;
 
 use \Classes\DB\Sql;
 use \Classes\Model;
+use \Classes\Mailer;
 	
 	class User extends Model{
 
 		const SESSION = "User";
+		const SECRET = "xxxxx"; //ocultar essa chave ao subir para o Git
 
 		public static function login($login, $password){
 
@@ -121,6 +123,112 @@ use \Classes\Model;
 			$sql->query("CALL sp_users_delete(:iduser)", array(
 				":iduser" => $this->getiduser()
 			));
+		}
+
+		public static function getForgot($email){
+			$sql = new Sql();
+
+			$results = $sql->select("SELECT * 
+				FROM tb_persons a 
+				INNER JOIN tb_users b USING(idperson) 
+				WHERE a.desemail = :email",
+				array(
+					":email" => $email
+			));
+
+			if (count($results) === 0) {
+				throw new \Exception("Não foi possível recuperar a senha");
+				
+			}else {
+
+				$data = $results[0];
+
+				$results2 = $sql->select("CALL sp_userspasswordsrecoveries_create(:iduser, :desip)", array(
+					":iduser" => $data["iduser"],
+					":desip" => $_SERVER["REMOTE_ADDR"]
+				));
+
+				if (count($results2) === 0) {
+					throw new \Exception("Não foi possível recuperar a senha");
+				}else{
+					
+					$dataRecovery = $results2[0];
+					$iv = random_bytes(openssl_cipher_iv_length('aes-256-cbc'));
+					$code = openssl_encrypt($dataRecovery['idrecovery'], 'aes-256-cbc', User::SECRET, 0, $iv);
+					$result = base64_encode($iv.$code);
+					
+					if ($data['inadmin'] === true) {
+						$link = "http://status.mav.com.br/admin/forgot/reset?code=$result";
+					} else {
+						$link = "http://status.mav.com.br/admin/forgot/reset?code=$result";
+					} 
+					$mailer = new Mailer($data['desemail'], $data['desperson'], "Redefinir senha da Pagina de Status", "forgot", array(
+						"name"=>$data['desperson'],
+						"link"=>$link
+					));
+					
+					
+					$mailer->send();
+
+					return $data;
+				}
+
+
+			}
+		}
+
+		public static function validForgotDecrypt($result){
+			
+			$result = base64_decode($result);
+
+			$code = mb_substr($result, openssl_cipher_iv_length('aes-256-cbc'), null, '8bit');
+
+			$iv = mb_substr($result, 0, openssl_cipher_iv_length('aes-256-cbc'), '8bit');;
+			
+			$idrecovery = openssl_decrypt($code, 'aes-256-cbc', User::SECRET, 0, $iv);
+			
+			$sql = new Sql();
+
+			$result = $sql->select("
+				SELECT * 
+				FROM tb_userspasswordsrecoveries a
+				INNER JOIN tb_users b USING(iduser)
+				INNER JOIN tb_persons c USING(idperson)
+				WHERE
+				a.idrecovery = :idrecovery
+				AND
+				a.dtrecovery IS NULL
+				AND
+				DATE_ADD(a.dtregister, INTERVAL 1 HOUR) >= NOW()", array(
+					":idrecovery" => $idrecovery
+				));
+
+				if(count($result)===0){
+					throw new \Exception("Não foi possível recuperar a senha");
+				}else {
+					return $result[0];
+				}
+			
+			
+		}
+
+		public static function setForgotUsed($idrecovery){
+			$sql = new Sql();
+
+			$sql->query("UPDATE tb_userspasswordsrecoveries SET dtrecovery = NOW()
+				WHERE idrecovery = :idrecovery", array(
+					":idrecovery" => $idrecovery
+				));
+		}
+
+		public function setPassword($password){
+			$sql = new Sql();
+
+			$sql->query("UPDATE tb_users SET despassword = :password WHERE 
+				iduser = :iduser", array(
+					":password" => $password,
+					":iduser" => $this->getiduser()
+				));
 		}
 	}
 ?>
